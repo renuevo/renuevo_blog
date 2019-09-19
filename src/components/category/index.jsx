57f56069e0db757c171257dbb847ba0514f43c27
renuevo <novelty0110@gmail.com>
2019-09-20 00:08:33 +0900

    blog init develop

--- src/components/category/index.jsx
+++ src/components/category/index.jsx
@@ -1,13 +1,85 @@
-import React from 'react'
+import React, {useState} from 'react'
 
-import { Item } from './item'
-import { rhythm } from '../../utils/typography'
+import Button from '@material-ui/core/Button'
+import Menu from '@material-ui/core/Menu'
+import TextField from '@material-ui/core/TextField'
 
 import './index.scss'
+import MenuItem from "@material-ui/core/MenuItem";
 
-export const Category = ({ categories, category, selectCategory }) => {
-  return (
-    <ul
+export const Category = ({categories, category, selectCategory}) => {
+    const [anchorEl, setAnchorEl] = useState(null)
+    const [filterCategory, setFilterCategory] = useState("All")
+
+    function handleClick(event) {
+        setAnchorEl(event.currentTarget);
+    }
+
+    function handleClose(title) {
+        setAnchorEl(null);
+        if (!(title instanceof Object))
+            setFilterCategory(title)
+    }
+
+    return (
+        <div
+            className="category-container"
+            role="tablist"
+            id="category"
+        >
+            <Button className="filter-button" aria-controls="simple-menu" aria-haspopup="true" onClick={handleClick}>
+                {filterCategory}
+            </Button>
+            <Menu
+                id="simple-menu"
+                anchorEl={anchorEl}
+                keepMounted
+                open={Boolean(anchorEl)}
+                onClose={handleClose}
+            >
+                <MenuItem
+                    aria-selected={category === "All" ? 'true' : 'false'}
+                    onClick={() => {
+                        selectCategory("All")
+                        handleClose("All")
+                    }}>All</MenuItem>
+
+                {categories.map((item, idx) => (
+                    <MenuItem
+                        key={idx}
+                        aria-selected={category === item ? 'true' : 'false'}
+                        onClick={() => {
+                            selectCategory(item)
+                            handleClose(item)
+                        }}>{item}</MenuItem>
+                ))}
+            </Menu>
+            <TextField
+                id="standard-search"
+                className="search-filter"
+                label="Search"
+                type="search"
+                margin="normal"
+            />
+        </div>
+    )
+}
+
+/*
+<Item title={'All'} category={category} selectCategory={selectCategory} handleClose={handleClose}/>
+{categories.map((item, idx) => (
+    <Item
+        key={idx}
+        title={item}
+        category={category}
+        selectCategory={selectCategory}
+        handleClose={handleClose}
+    />
+))}*/
+
+
+/*
+*   <ul
       className="category-container"
       role="tablist"
       id="category"
@@ -25,5 +97,4 @@ export const Category = ({ categories, category, selectCategory }) => {
         />
       ))}
     </ul>
-  )
-}
+* */
