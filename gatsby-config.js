feeae8023f605a00c1d0a823ff5e952e595371fd
renuevo <renuevo0110@gmail.com>
2020-03-05 00:20:46 +0900

    Add External Link :sparkles:

--- gatsby-config.js
+++ gatsby-config.js
@@ -63,6 +63,7 @@ module.exports = {
           `gatsby-remark-smartypants`,
           `gatsby-remark-autolink-headers`,
           `gatsby-remark-emoji`,
+          "gatsby-remark-external-links",
         ],
       },
     },
